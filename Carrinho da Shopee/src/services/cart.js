5b159e94a8c3061872dd16dde3120acc5e7a516d
Thatacorekook902 <167568065+Thatacorekook902@users.noreply.github.com>
2026-04-27 11:34:42 -0300

    Update cart.js

--- Carrinho da Shopee/src/services/cart.js
+++ Carrinho da Shopee/src/services/cart.js
@@ -1,19 +1,29 @@
-//quais ações meu carrinho pode fazer 
+//quais açoes meu carrinho pode fazer 
 
-//casos de uso 
-//-> adicionar item no carrinho
-async function addItem(useCart){
-
-}
-    
-//-> deletar item do carrinho
-async function  deleteItem (useCart, name) {
-    
+//casos de uso
+//✅ -> adicionar item no carrinho
+async function addItem(userCart , item) {
+    userCart.push(item);
 }
 
-//-> remover um item
-async function  removeItem(useCart, index) {
-    
+//-> calcular o total do carrinho
+async function calculateTotal(useCart) {
+   return userCart.reduce((total, item) => total + item.subtotal(), 0);
 }
 
-//-> calcular o total
+//-> deletar item do carrinho 
+async function deleteItem(userCart, name) {
+
+}
+
+//-> remover um item- diminui um item
+async function removeItem(usercart, index) {
+
+}
+
+export {
+    addItem,
+    calculateTotal,
+    deleteItem,
+    removeItem,
+}
