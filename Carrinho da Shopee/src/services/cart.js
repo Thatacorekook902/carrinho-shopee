82d885521956de801c9e44d56921f5e5e52a301e
Thatacorekook902 <167568065+Thatacorekook902@users.noreply.github.com>
2026-04-27 07:43:58 -0300

    Add files via upload

---
+++ Carrinho da Shopee/src/services/cart.js
@@ -0,0 +1,19 @@
+//quais ações meu carrinho pode fazer 
+
+//casos de uso 
+//-> adicionar item no carrinho
+async function addItem(useCart){
+
+}
+    
+//-> deletar item do carrinho
+async function  deleteItem (useCart, name) {
+    
+}
+
+//-> remover um item
+async function  removeItem(useCart, index) {
+    
+}
+
+//-> calcular o total
