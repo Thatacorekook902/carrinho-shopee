807da55bd187564a0ca1374919788c7d8d0a395a
Thatacorekook902 <167568065+Thatacorekook902@users.noreply.github.com>
2026-04-29 09:45:11 -0300

    Update index.js

--- Carrinho da Shopee/src/index.js
+++ Carrinho da Shopee/src/index.js
@@ -1,10 +1,15 @@
+import * as cartService from "./services/cart.js";
 import createItem from "./services/item.js";
 
-const cart =[];
+const myCart =[];
 
 console.log("Welcome to the your Shopee Cart!");
 
 const item1 =  await createItem("hotwheels ferrari", 20.99, 1);
 const item2 =  await createItem("hotwheels lamborghini", 39.99, 3);
 
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myCart, item2);
+
+await cartService.calculateTotal();
 console.log(item2.subtotal());
